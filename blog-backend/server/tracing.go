775766bbb24949f0cef05326f27e9da9daaf6a11
GHjiejie <3426571530@qq.com>
2024-11-07 03:09:10 +0800

    feat:tracing 中间件调整

--- blog-backend/server/tracing.go
+++ blog-backend/server/tracing.go
@@ -2,9 +2,14 @@ package server
 
 import (
 	"blog-backend/pkg/casbinpermit"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/golang/protobuf/ptypes/any"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // 设置无需验证的路径
@@ -22,18 +27,28 @@ const (
 	ContentTypeHeaderVal   = "application/json"
 )
 
+// ErrorBody 用于格式化 API 返回的错误信息，包含错误、消息、状态码及详细信息。这使得客户端能够更好地理解发生的错误。
+type ErrorBody struct {
+	Error   string     `protobuf:"bytes,1,name=error" json:"error"`
+	Message string     `protobuf:"bytes,1,name=message" json:"message"`
+	Code    int32      `protobuf:"varint,2,name=code" json:"code"`
+	Details []*any.Any `protobuf:"bytes,3,rep,name=details" json:"details,omitempty"`
+}
+
 func (s *BlogServer) Tracing(nextHandle http.Handler, userPermit *casbinpermit.Permit) http.Handler {
+	logger := log.WithFields(log.Fields{
+		"module": "Tracing",
+	})
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 从请求体中获取用户信息
-		username := r.Header.Get("username")
-		log.Infof("username: %s", username)
+		// username := r.Header.Get("username")
 		// 判断当前接口是否需要进行鉴权验证
 		if needAuthorizations(r.URL.Path, r.Method) {
 			var (
 				username  string
 				userToken string
 			)
-			log.Infof("当前接口需要进行鉴权验证,同时需要进行权限检查")
+			logger.Infof("当前接口需要进行鉴权验证,同时需要进行权限检查")
 			// 前端在登录成功后，会在下一次请求的时候将token设置在请求头里面，所以我们需要从请求头中获取token
 			authHeader := r.Header.Get(AuthorizationHeaderKey)
 			// 也可以从请求参数中获取token（这个主要是用于调试？）
@@ -47,52 +62,115 @@ func (s *BlogServer) Tracing(nextHandle http.Handler, userPermit *casbinpermit.P
 			// claims, err := s.auth.ParseUserToken(userToken)
 			claims, err := s.auth.ParseUserToken(userToken)
 			if err != nil {
-				log.Errorf("token解析失败, err: %v", err)
+				logger.Errorf("token解析失败, err: %v", err)
 				w.WriteHeader(http.StatusInternalServerError)
-				// 返回错误消息给前端
 				return
 			}
 			if claims == nil {
-				log.Error("token解析失败, claims为空")
+				logger.Error("token解析失败, claims为空")
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 			username = claims.Username
-			log.Infof("token解析成功, 输出获取的claims: %v", claims)
+			logger.Infof("token解析成功, 输出获取的claims: %v", claims)
 
 			// 我们直接进行权限验证
 			permitted, err := userPermit.CheckPermission(username, r.URL.Path, r.Method)
 			if err != nil {
-				log.Errorf("权限验证失败, err: %v", err)
+				log.Infof("user(%s) permission 1st checking result, permit(%v)", username, permitted)
+				newerr := status.New(codes.Unknown, err.Error())
+				// 将错误信息返回给前端
+				body := &ErrorBody{
+					Error:   newerr.Message(),
+					Message: newerr.Message(),
+					Code:    int32(newerr.Code()),
+					Details: newerr.Proto().GetDetails(),
+				}
+				msg, err := json.Marshal(body)
+				if err != nil {
+					log.Errorf("failed to marshal body with err(%s)", err.Error())
+
+				}
+				w.Header().Set(ContentTypeHeaderKey, ContentTypeHeaderVal)
 				w.WriteHeader(http.StatusInternalServerError)
+				_, err = w.Write(msg)
+				if err != nil {
+					logger.Errorf("failed to write msg with err(%s)", err.Error())
+				}
 				return
 			}
+			if permitted {
+				logger.Infof("user(%s) permission 1st checking result, permit(%v)", username, permitted)
+				nextHandle.ServeHTTP(w, r)
+			} else {
+				if err := userPermit.Enforcer.LoadPolicy(); err != nil {
+					isOk, _ := userPermit.CheckPermission(username, r.URL.Path, r.Method)
+					logger.Debugf("user(%s) permission 2nd checking result, permit(%v)", username, isOk)
+					if isOk {
+						nextHandle.ServeHTTP(w, r)
+					}
+				}
+				logger.Errorf("%s does not have permission to %s %s", username, r.Method, r.URL.Path)
+				newerr := status.New(
+					codes.PermissionDenied,
+					fmt.Sprintf("%s does not have permission to %s %s", username, r.Method, r.URL.Path),
+				)
+				body := ErrorBody{
+					Error:   newerr.Message(),
+					Message: newerr.Message(),
+					Code:    int32(newerr.Code()),
+					Details: newerr.Proto().GetDetails(),
+				}
+				msg, err := json.Marshal(body)
+				if err != nil {
+					logger.Errorf("marshal error body failed with err(%s)", err.Error())
+				}
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusForbidden)
+				_, err = w.Write(msg)
+				if err != nil {
+					logger.Errorf("failed to write msg with err(%s)", err.Error())
+				}
+				return
+			}
+			// nextHandle.ServeHTTP(w, r)
 
 			// 如果首次校验失败,则重新从数据库加载策略,然后再次进行校验
-			if !permitted {
-				if err := userPermit.Enforcer.LoadPolicy(); err != nil {
-					log.Errorf("failed to load policy with err(%s)", err.Error())
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-				permitted, err = userPermit.CheckPermission(username, r.URL.Path, r.Method)
-				if err != nil {
-					log.Errorf("权限验证失败, err: %v", err)
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-			}
-
-			log.Infof("user(%s) permission 1st checking result, permit(%v)", username, permitted)
-
-			// 进行鉴权验证
-			// TODO
+			// if !permitted {
+			// 	if err := userPermit.Enforcer.LoadPolicy(); err != nil {
+			// 		isOk, _ := userPermit.CheckPermission(username, r.URL.Path, r.Method)
+			// 		logger.Debugf("user(%s) permission 2nd checking result, permit(%v)", username, isOk)
+			// 		if isOk {
+			// 			nextHandle.ServeHTTP(w, r)
+			// 		}
+			// 	}
+			// 	logger.Errorf("%s does not have permission to %s %s", username, r.Method, r.URL.Path)
+			// 	newerr := status.New(
+			// 		codes.PermissionDenied,
+			// 		fmt.Sprintf("%s does not have permission to %s %s", username, r.Method, r.URL.Path),
+			// 	)
+			// 	body := ErrorBody{
+			// 		Error:   newerr.Message(),
+			// 		Message: newerr.Message(),
+			// 		Code:    int32(newerr.Code()),
+			// 		Details: newerr.Proto().GetDetails(),
+			// 	}
+			// 	msg, err := json.Marshal(body)
+			// 	if err != nil {
+			// 		logger.Errorf("marshal error body failed with err(%s)", err.Error())
+			// 	}
+			// 	w.Header().Set("Content-Type", "application/json")
+			// 	w.WriteHeader(http.StatusForbidden)
+			// 	_, err = w.Write(msg)
+			// 	if err != nil {
+			// 		logger.Errorf("failed to write msg with err(%s)", err.Error())
+			// 	}
+			// 	return
+			// }
 		} else {
-
 			log.Info("当前接口无需进行鉴权验证")
+			nextHandle.ServeHTTP(w, r)
 		}
-		log.Info("接下来将请求转发给下一个处理函数")
-		nextHandle.ServeHTTP(w, r)
 	})
 }
 
